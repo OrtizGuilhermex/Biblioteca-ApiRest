b1f9912dcbcc72374171655c41190fcea2f8ff1b
luiz_gf_ortiz <luiz_gf_ortiz@estudante.sesisenai.org.br>
2026-02-19 15:36:05 -0300

    feat: adiciona metodos CRUD em UsuarioService

---
+++ src/main/java/com/api/biblioteca/service/UsuarioService.java
@@ -0,0 +1,38 @@
+package com.api.biblioteca.service;
+
+import com.api.biblioteca.model.Usuario;
+import com.api.biblioteca.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@Service
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public Usuario salvarUsuario(Usuario usuario)throws SQLException{
+        return usuarioRepository.salvarUsuario(usuario);
+    }
+
+    public List<Usuario> obterTodosUsuarios()throws SQLException{
+        return usuarioRepository.obterTodosUsuarios();
+    }
+
+    public Usuario obterUsuarioPorID(int id)throws SQLException{
+        return usuarioRepository.obterUsuarioPorID(id);
+    }
+
+    public Usuario atualizarUSuario (Usuario usuario) throws SQLException {
+        return usuarioRepository.atualizarUSuario(usuario);
+    }
+
+    public void deletarUsuario(int id) throws SQLException{
+        usuarioRepository.deletarUsuario(id);
+    }
+}
