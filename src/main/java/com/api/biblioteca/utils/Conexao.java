ed62b24a7e1dca786558b02ee082e6ada614864f
luiz_gf_ortiz <luiz_gf_ortiz@estudante.sesisenai.org.br>
2026-02-19 14:18:14 -0300

    feat: Adiciona conexao com bd

---
+++ src/main/java/com/api/biblioteca/utils/Conexao.java
@@ -0,0 +1,17 @@
+package com.api.biblioteca.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+
+    private static final String URL = "jdbc:mysql://localhost:3356/biblioteca?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWD = "mysqlPW";
+
+    public Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL,USER,PASSWD);
+    }
+
+}
