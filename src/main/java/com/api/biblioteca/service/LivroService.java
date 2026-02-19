d5c807a27c5c08a016fe19cd4d3f3eb06a2d918f
luiz_gf_ortiz <luiz_gf_ortiz@estudante.sesisenai.org.br>
2026-02-19 15:40:49 -0300

    feat: adiciona metodos CRUD em LivroService

---
+++ src/main/java/com/api/biblioteca/service/LivroService.java
@@ -0,0 +1,40 @@
+package com.api.biblioteca.service;
+
+
+import com.api.biblioteca.model.Livro;
+import com.api.biblioteca.repository.LivroRepository;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@Service
+public class LivroService {
+
+    private final LivroRepository livroRepository;
+
+    public LivroService(LivroRepository livroRepository) {
+        this.livroRepository = livroRepository;
+    }
+
+    public Livro salvarLivro(Livro livro) throws SQLException{
+        return livroRepository.salvarUsuario(livro);
+    }
+
+    public List<Livro> obterTodosLivros() throws SQLException{
+        return livroRepository.obterTodoslivros();
+    }
+
+    public Livro obterLivroPorID(int id) throws SQLException{
+        return livroRepository.obterLivroPorID(id);
+    }
+
+    public Livro atualizarLivro(Livro livro) throws SQLException{
+        return livroRepository.atualizarUSuario(livro);
+    }
+
+    public void deletarLivro(int id) throws SQLException{
+        livroRepository.deletarLivro(id);
+    }
+
+}
