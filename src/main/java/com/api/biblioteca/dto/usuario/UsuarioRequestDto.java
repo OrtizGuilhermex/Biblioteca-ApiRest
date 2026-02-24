189ebc97a91c85b419991436275d4beab01d6279
luiz_gf_ortiz <luiz_gf_ortiz@estudante.sesisenai.org.br>
2026-02-24 19:14:30 -0300

    feat: adiciona UsuarioRequestDto/responseDto

---
+++ src/main/java/com/api/biblioteca/dto/usuario/UsuarioRequestDto.java
@@ -0,0 +1,7 @@
+package com.api.biblioteca.dto.usuario;
+
+public record UsuarioRequestDto(
+        String nome,
+        String email
+) {
+}
---
+++ src/main/java/com/api/biblioteca/dto/usuario/UsuarioResponseDto.java
@@ -0,0 +1,8 @@
+package com.api.biblioteca.dto.usuario;
+
+public record UsuarioResponseDto(
+        int id,
+        String nome,
+        String email
+) {
+}
